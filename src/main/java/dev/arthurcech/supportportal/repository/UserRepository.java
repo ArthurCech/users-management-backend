de689766de5346f7048ce838cd1d7da3f2552f37
Arthur Cech Francisco <arthurcech@gmail.com>
2022-07-25 10:32:09 -0300

    feat: add repository for User

---
+++ src/main/java/dev/arthurcech/supportportal/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.arthurcech.supportportal.repository;
+
+import dev.arthurcech.supportportal.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+
+}
