aa0a9487d944c2c19cb6c9a153dc65894d4855c2
Arthur Cech Francisco <arthurcech@gmail.com>
2022-07-26 06:48:47 -0300

    feat: add methods to UserService - register user, get all users, get user by username and get user by email

--- src/main/java/dev/arthurcech/supportportal/service/UserService.java
+++ src/main/java/dev/arthurcech/supportportal/service/UserService.java
@@ -1,4 +1,20 @@
 package dev.arthurcech.supportportal.service;
 
+import dev.arthurcech.supportportal.domain.User;
+import dev.arthurcech.supportportal.exception.domain.EmailExistException;
+import dev.arthurcech.supportportal.exception.domain.UserNotFoundException;
+import dev.arthurcech.supportportal.exception.domain.UsernameExistException;
+
+import java.util.List;
+
 public interface UserService {
+
+    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException;
+
+    List<User> getUsers();
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+
 }
